f3a7f47f5efc9c1883b0e19d0115e754da0fd8a0
Zdevick <zevick@hotmail.com>
2026-02-13 17:52:33 -0500

    Update version.h

--- DialogueInterceptor/include/version.h
+++ DialogueInterceptor/include/version.h
@@ -2,5 +2,5 @@
 
 #define PLUGIN_NAME "DialogueInterceptor"
 #define PLUGIN_VERSION "0.1.0"
-#define PLUGIN_AUTHOR "zevck"
-#define PLUGIN_DESCRIPTION "Intercepts and blocks dialogue before audio/subtitle plays"
+#define PLUGIN_AUTHOR "Zevick"
+#define PLUGIN_DESCRIPTION "Blocks dialogue from being logged by SkyrimNet"
